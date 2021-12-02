b6a39bfc037fc9ea2b5f118d10ccd4444a928615
Osama Asif <osama.asif20@gmail.com>
2021-12-02 14:25:42 +0500

    * Calculator screen overflow issue fixed

--- lib/screens/calculator/calculator_screen.dart
+++ lib/screens/calculator/calculator_screen.dart
@@ -24,79 +24,103 @@ class CalculatorScreen extends StatelessWidget {
             alignment: Alignment.topRight,
             child: SvgPicture.asset(AssetConstants.topRightRoundCircle),
           ),
-          Image.asset(AssetConstants.tax),
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 15, top: 35, right: 15, bottom: 20),
-            child: Text(
-              StringConstants.whatIsYourAnnualIncomeGoal,
-              textAlign: TextAlign.left,
-              style: Theme.of(context)
-                  .textTheme
-                  .headline5!
-                  .copyWith(fontWeight: FontWeight.w700, fontSize: 28),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelStyle: TextStyle(
-                  fontSize: 16.0,
-                  fontFamily: 'helvetica',
-                  fontStyle: FontStyle.normal,
-                  color: ColorConstants.black.withOpacity(0.4),
-                ),
-                label: const Text(StringConstants.annualIncom),
-                filled: false,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  borderSide: BorderSide(
-                    color: ColorConstants.black.withOpacity(0.5),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  borderSide: BorderSide(
-                    color: ColorConstants.black.withOpacity(0.5),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 15, top: 15, right: 15, bottom: 15),
-              child: Text(
-                StringConstants.estimatedPrice,
-                textAlign: TextAlign.left,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText1!
-                    .copyWith(fontWeight: FontWeight.w500, fontSize: 18),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.green),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Center(
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.asset(AssetConstants.tax),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15, top: 35, right: 15, bottom: 20),
                     child: Text(
-                  "122euros",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6!
-                      .copyWith(fontWeight: FontWeight.w500, fontSize: 25),
-                )),
+                      StringConstants.whatIsYourAnnualIncomeGoal,
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5!
+                          .copyWith(fontWeight: FontWeight.w700, fontSize: 28),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelStyle: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'helvetica',
+                          fontStyle: FontStyle.normal,
+                          color: ColorConstants.black.withOpacity(0.4),
+                        ),
+                        label: const Text(StringConstants.annualIncom),
+                        filled: false,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          borderSide: BorderSide(
+                            color: ColorConstants.black.withOpacity(0.5),
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          borderSide: BorderSide(
+                            color: ColorConstants.black.withOpacity(0.5),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 15, top: 15, right: 15, bottom: 15),
+                      child: Text(
+                        StringConstants.estimatedPrice,
+                        textAlign: TextAlign.left,
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                            fontWeight: FontWeight.w500, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Center(
+                            child: Text(
+                          "122euros",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(
+                                  fontWeight: FontWeight.w500, fontSize: 25),
+                        )),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 15),
+                    child: ElevatedButton(
+                      style: ElevatedButtonTheme.of(context).style?.copyWith(
+                            minimumSize: MaterialStateProperty.all(
+                              Size(MediaQuery.of(context).size.width, 70),
+                            ),
+                          ),
+                      onPressed: () {},
+                      child: const Text(
+                        StringConstants.orderNew,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           )

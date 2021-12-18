de993d12bf4e4ebf543505ebc122d67c12140364
osamaasifoak <osama.asif20@gmail.com>
2021-12-18 17:59:49 +0500

    * Faq screen completed

--- lib/screens/faq/faq_screen.dart
+++ lib/screens/faq/faq_screen.dart
@@ -1,127 +1,199 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/src/public_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:steuermachen/components/app_bar/appbar_with_side_corner_circle_and_body.dart';
+import 'package:steuermachen/components/error_component.dart';
+import 'package:steuermachen/components/loading_component.dart';
 import 'package:steuermachen/constants/colors/color_constants.dart';
 import 'package:steuermachen/constants/styles/font_styles_constants.dart';
 import 'package:steuermachen/languages/locale_keys.g.dart';
+import 'package:steuermachen/main.dart';
+import 'package:steuermachen/wrappers/faq_wrapper.dart';
 
-class FaqScreen extends StatefulWidget {
+class FaqScreen extends StatelessWidget {
   const FaqScreen({Key? key}) : super(key: key);
-
-  @override
-  State<FaqScreen> createState() => _FaqScreenState();
-}
-
-class _FaqScreenState extends State<FaqScreen> {
-  List<Map<String, dynamic>> faqItem = [
-    {"title": LocaleKeys.faqItem1.tr(), "active": false, "route": ""},
-    {"title": LocaleKeys.faqItem2.tr(), "active": false, "route": ""},
-    {"title": LocaleKeys.faqItem3.tr(), "active": false, "route": ""},
-    {"title": LocaleKeys.faqItem4.tr(), "active": false, "route": ""},
-    {"title": LocaleKeys.faqItem5.tr(), "active": false, "route": ""},
-    {"title": LocaleKeys.faqItem6.tr(), "active": false, "route": ""},
-    {"title": LocaleKeys.faqItem7.tr(), "active": false, "route": ""},
-  ];
   @override
   Widget build(BuildContext context) {
     return AppBarWithSideCornerCircleAndRoundBody(
       body: SingleChildScrollView(
+          child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: FutureBuilder<DocumentSnapshot>(
+            future: firestore.collection("faq").doc("content").get(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                Map<String, dynamic> x =
+                    snapshot.data.data() as Map<String, dynamic>;
+                FAQContentWrapper res = FAQContentWrapper.fromJson(x);
+                return _FAQListTile(
+                  faqContentWrapper: res,
+                );
+              } else if (snapshot.hasError) {
+                return const ErrorComponent();
+              } else {
+                return const LoadingComponent();
+              }
+            }),
+      )),
+    );
+  }
+}
+
+class _FAQListTile extends StatefulWidget {
+  const _FAQListTile({Key? key, required this.faqContentWrapper})
+      : super(key: key);
+  final FAQContentWrapper faqContentWrapper;
+  @override
+  __FAQListTileState createState() => __FAQListTileState();
+}
+
+class __FAQListTileState extends State<_FAQListTile> {
+  late FAQContentWrapper _wrapper;
+
+  @override
+  void initState() {
+    super.initState();
+    _wrapper = widget.faqContentWrapper;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const divider = Padding(
+      padding: EdgeInsets.symmetric(horizontal: 18),
+      child: Divider(
+        thickness: 1,
+        height: 4,
+      ),
+    );
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Text(
+            LocaleKeys.frequentlyAskedQuestion.tr(),
+            style:
+                Theme.of(context).textTheme.headline6!.copyWith(fontSize: 22),
+          ),
+        ),
+        if (context.locale == const Locale('en'))
+          for (var i = 0; i < _wrapper.en!.length; i++)
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    if (_wrapper.en![i].isActive!) {
+                      _wrapper.en![i].isActive = false;
+                    } else {
+                      _wrapper.en![i].isActive = true;
+                    }
+                  });
+                },
+                child: questionListTile(_wrapper.en![i], divider))
+        else
+          for (var i = 0; i < _wrapper.du!.length; i++)
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    if (_wrapper.du![i].isActive!) {
+                      _wrapper.du![i].isActive = false;
+                    } else {
+                      _wrapper.du![i].isActive = true;
+                    }
+                  });
+                },
+                child: questionListTile(_wrapper.du![i], divider))
+      ],
+    );
+  }
+
+  Container questionListTile(FAQContent faq, Padding divider) {
+    return Container(
+      color: faq.isActive!
+          ? ColorConstants.formFieldBackground
+          : ColorConstants.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _FAQQuestion(
+            faq: faq,
+          ),
+          divider,
+          _FAQAnswer(
+            faq: faq,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FAQQuestion extends StatelessWidget {
+  const _FAQQuestion({
+    Key? key,
+    required this.faq,
+  }) : super(key: key);
+
+  final FAQContent faq;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        faq.question!,
+        style: Theme.of(context)
+            .textTheme
+            .bodyText2!
+            .copyWith(fontSize: 16, color: ColorConstants.green),
+      ),
+      trailing: const Icon(
+        Icons.keyboard_arrow_down_outlined,
+        size: 32,
+      ),
+    );
+  }
+}
+
+class _FAQAnswer extends StatelessWidget {
+  const _FAQAnswer({
+    Key? key,
+    required this.faq,
+  }) : super(key: key);
+
+  final FAQContent faq;
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: faq.isActive!,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text(
+              faq.title!,
+              style: FontStyles.fontMedium(
+                  fontSize: 17, fontWeight: FontWeight.w700),
+            ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
+              padding: const EdgeInsets.only(bottom: 15),
               child: Text(
-                LocaleKeys.frequentlyAskedQuestion.tr(),
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6!
-                    .copyWith(fontSize: 22),
+                faq.answer!,
+                style: FontStyles.fontMedium(
+                    fontSize: 17, fontWeight: FontWeight.w500),
               ),
             ),
-            for (var i = 0; i < faqItem.length; i++)
-              Container(
-                color: faqItem[i]["active"]!
-                    ? ColorConstants.formFieldBackground
-                    : ColorConstants.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        for (var element in faqItem) {
-                          element["active"] = false;
-                        }
-                        setState(() {
-                          if (!faqItem[i]["active"]) {
-                            faqItem[i]["active"] = true;
-                          } else {
-                            faqItem[i]["active"] = false;
-                          }
-                        });
-                      },
-                      child: ListTile(
-                        title: Text(
-                          faqItem[i]["title"]!,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText2!
-                              .copyWith(
-                                  fontSize: 16, color: ColorConstants.green),
-                        ),
-                        trailing: const Icon(
-                          Icons.keyboard_arrow_down_outlined,
-                          size: 32,
-                        ),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 18),
-                      child: Divider(
-                        thickness: 1,
-                        height: 4,
-                      ),
-                    ),
-                    Visibility(
-                      visible: faqItem[i]["active"]!,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 18),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "1.1. Procedure for an order",
-                              style: FontStyles.fontMedium(
-                                  fontSize: 17, fontWeight: FontWeight.w700),
-                            ),
-                            Text(
-                              "Rhe easy way to your tax return",
-                              style: FontStyles.fontMedium(
-                                  fontSize: 17, fontWeight: FontWeight.w500),
-                            ),
-                            Text(
-                              "You will immediately receive a checklist by email with which you can sort your tax-relevant documents yourself.",
-                              style: FontStyles.fontMedium(
-                                  fontSize: 15, fontWeight: FontWeight.w500),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              child: Text(
-                                "Read more",
-                                style: FontStyles.fontMedium(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: ColorConstants.primary),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(vertical: 10),
+            //   child: Text(
+            //     "Read more",
+            //     style: FontStyles.fontMedium(
+            //         fontSize: 15,
+            //         fontWeight: FontWeight.bold,
+            //         color: ColorConstants.primary),
+            //   ),
+            // ),
           ],
         ),
       ),
---
+++ lib/wrappers/faq_wrapper.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+class FAQContentWrapper {
+  FAQContentWrapper({
+    this.du,
+    this.en,
+  });
+
+  List<FAQContent>? du;
+  List<FAQContent>? en;
+
+  factory FAQContentWrapper.fromRawJson(String str) =>
+      FAQContentWrapper.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory FAQContentWrapper.fromJson(Map<String, dynamic> json) =>
+      FAQContentWrapper(
+        du: json["du"] == null
+            ? null
+            : List<FAQContent>.from(
+                json["du"].map((x) => FAQContent.fromJson(x))),
+        en: json["en"] == null
+            ? null
+            : List<FAQContent>.from(
+                json["en"].map((x) => FAQContent.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "du":
+            du == null ? null : List<dynamic>.from(du!.map((x) => x.toJson())),
+        "en":
+            en == null ? null : List<dynamic>.from(en!.map((x) => x.toJson())),
+      };
+}
+
+class FAQContent {
+  FAQContent({
+    this.question,
+    this.title,
+    this.answer,
+    this.isActive
+  });
+
+  String? question;
+  String? title;
+  String? answer;
+  bool? isActive;
+  factory FAQContent.fromRawJson(String str) =>
+      FAQContent.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory FAQContent.fromJson(Map<String, dynamic> json) => FAQContent(
+        question: json["question"],
+        title: json["title"],
+        answer: json["answer"],
+        isActive: false,
+      );
+
+  Map<String, dynamic> toJson() => {
+        "question": question,
+        "title": title,
+        "answer": answer,
+      };
+}
